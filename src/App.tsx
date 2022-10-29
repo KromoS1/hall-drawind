ef278b3576b672942fd470f97a0d265026b138bb
Krom_oS1 <Roma@bezkassira.by>
2022-10-29 21:40:35 +0300

    add bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,46 @@
-import React from 'react'
-import './App.css'
+import React from 'react';
+import './App.css';
 
 function App() {
-    const a = (name: string) => {
-        console.log(1)
-
-        const f = [ 'w', 'w' ]
-    }
 
     return (
-        <div className="App">
-            <header className="App-header">Hello</header>
+        <div className={"show-scrollbar layout-fixed"}>
+            <div className={"wrapper"}>
+                <header className={"topnavbar-wrapper"}>
+                    <nav className={"navbar topnavbar"}>
+                        <ul className="navbar-nav mr-auto flex-row">
+                            <li className="nav-item">
+                                <a className="nav-link d-none d-md-block d-lg-block d-xl-block" href="#" data-trigger-resize="" data-toggle-state="aside-collapsed">
+                                    <em className="fas fa-bars"/>
+                                </a>
+                                <a className="nav-link sidebar-toggle d-md-none" href="#" data-toggle-state="aside-toggled" data-no-persist="true">
+                                    <em className="fas fa-bars"/>
+                                </a>
+                            </li>
+                        </ul>
+                        <ul className={"navbar-nav flex-row"}>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#" data-toggle-state="offsidebar-open" data-no-persist="true">
+                                    <em className="fas fa-bars"/>
+                                </a>
+                            </li>
+                        </ul>
+                    </nav>
+                </header>
+                <aside className="aside-container">
+                    <div className="aside-inner">
+                        <nav className="sidebar" data-sidebar-anyclick-close=""></nav>
+                    </div>
+                </aside>
+                <aside className={"aside-container"}>
+                    <div className={"aside-inner"}>
+                        <nav className={"sidebar"} data-sidebar-anyclick-close=""></nav>
+                    </div>
+                </aside>
+                <section className="section-container">
+
+                </section>
+            </div>
         </div>
     )
 }
